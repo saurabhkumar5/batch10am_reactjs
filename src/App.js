2651ebb7634babf1aa96e65ac2881f094366ec29
saurabh kumar <saurabhkumar9493@gmail.com>
2024-06-14 11:05:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+     return(
+         <div>
+          <h1 style={{backgroundColor:'red',color:'blue'}}> hello </h1>
+          <h1>world</h1>
+          <p>hello how are you</p> 
+          <img src="https://img1.wsimg.com/isteam/ip/1172a89b-094a-415b-99fe-cc1e7d3cd9b0/blob.png/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:1500,h:1000,cg:true" />
+         </div>
+
+     )
 }
 
 export default App;
